6146a1b0a49fdffc0d093f7c87bd7afdc7fb1a92
RodrigoGemaque <ro.gemaque16@gmail.com>
2021-01-29 16:57:18 -0300

    Factory for system_requirement

---
+++ spec/factories/system_requirements.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :system_requirement do
+    sequence(:name){ |n|"Basic#{n}" }
+    operational_system { Faker::Computer.os }
+    
+    storage { "500GB" }
+    processor { "Amd Rizen 7" }
+    memory { "8 GB" }
+    video_board { "Geforce 1660" }
+  end
+end
