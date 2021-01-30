1484150119453e6d2c66ca4dc32aa8db49d3db63
RodrigoGemaque <ro.gemaque16@gmail.com>
2021-01-29 22:52:02 -0300

    Relationship has_many and thorough

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,6 @@ class Product < ApplicationRecord
   validates :price, presence: true, numericality: {greater_than: 0}
 
 
+  has_many :product_categories, dependent: :destroy
+  has_many :categories, through: :product_categories
 end
