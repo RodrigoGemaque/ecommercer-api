044fa781dc3e7b0e33708edf5d373bd713f36881
RodrigoGemaque <ro.gemaque16@gmail.com>
2021-01-30 20:38:01 -0300

    change name duedate for field due_date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_30_021633) do
+ActiveRecord::Schema.define(version: 2021_01_30_232501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2021_01_30_021633) do
     t.string "code"
     t.integer "status"
     t.decimal "discount_value", precision: 5, scale: 2
-    t.datetime "duedate"
+    t.datetime "due_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
