ab38e079752661123efcb8c955dc7fb7bc359668
Rodrigo <ro.gemaque16@gmail.com>
2021-02-04 17:14:32 -0300

    update action for categories with render errors

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -7,19 +7,32 @@ module Admin::V1
     def create
       @category = Category.new
       @category.attributes = category_params
+      save_category!
       
-      @category.save!
-      render :show
-    rescue
-      render json: { errors: {fields:  @category.errors.messages} } ,status: :unprocessable_entity
     end
 
+    def edit
+    end
+
+    def update 
+      @category = Category.find(params[:id])
+      @category.attributes = category_params
+      save_category!
+    end
+    
 
     private
 
     def category_params
       return {} unless params.has_key?(:category)
       params.require(:category).permit(:id, :name)
+    end
+
+    def save_category!
+      @category.save!
+      render :show
+    rescue
+      render_error(fields: @category.errors.messages) 
     end
 
   end
