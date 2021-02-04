fec1627d18e8706bdecd1b3d737af06c9edda53a
Rodrigo <ro.gemaque16@gmail.com>
2021-02-04 17:14:02 -0300

    method for render errors messages for controllers

--- app/controllers/admin/v1/api_controller.rb
+++ app/controllers/admin/v1/api_controller.rb
@@ -1,5 +1,13 @@
 module Admin::V1
   class ApiController < ApplicationController
     include Authenticable
+
+    def render_error(message: nil, fields: nil, status: :unprocessable_entity)
+      errors = {}
+      errors['fields'] = fields if fields.present? #so executa se o fields.present for verdadeiro
+      errors['message'] = message if message.present? #so executa se o fields.present for verdadeiro
+      render json: {errors: errors}, status: status
+    end
+
   end
 end
