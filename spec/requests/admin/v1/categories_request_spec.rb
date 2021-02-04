4a7acce9f90f7fa8c90e7d538565fc5c8de07a86
Rodrigo <ro.gemaque16@gmail.com>
2021-02-04 10:25:46 -0300

    test POST  in category

--- spec/requests/admin/v1/categories_request_spec.rb
+++ spec/requests/admin/v1/categories_request_spec.rb
@@ -3,12 +3,10 @@ require 'rails_helper'
 RSpec.describe "Admin::V1::Categories", type: :request do
   let(:user) { create(:user) }  
   
-
+  
   context  "GET /categories" do
     let(:url) {"/admin/v1/categories"} 
-
-    # seta para  a variavel url o caminho  para teste de contexto de get
-    
+    # seta para  a variavel url o caminho  para teste de contexto de get    
     let!(:categories) { create_list(:category, 5) } # cria uma variavel categories para pegar do factory bot 5 categorias
 
     it "return all Categories" do 
@@ -20,8 +18,60 @@ RSpec.describe "Admin::V1::Categories", type: :request do
       get url, headers: auth_header(user)
       expect(response).to have_http_status(:ok)
     end
+  end
+
+  context  "POST /categories" do
+    let(:url) {"/admin/v1/categories"} 
+
+    context "with a valid params" do
+      # devolve apenas os atributos ques estão no factory bot
+      let(:category_params) { { category: attributes_for(:category) }.to_json }
+      it 'adds a new Category' do 
+        expect do
+          post url, headers: auth_header(user), params: category_params
+        end.to change(Category, :count).by(1)   
+      end
+      
+      it 'returns last added Category' do
+        post url, headers: auth_header(user), params: category_params
+        expected_category = Category.last.as_json(only: %i(id name))
+        expect(body_json['category']).to eq expected_category
+      end
+
+      it "returns succes status  " do
+        post url, headers: auth_header(user), params: category_params
+        expect(response).to have_http_status(:ok)
+      end
 
 
+    end
+
+
+    context "with a invalid params" do
+      
+      let(:category_invalid_params) do 
+        { category: attributes_for(:category, name: nil) }.to_json
+      end
+
+
+      it 'does not add a new Category' do
+        expect do
+          post url, headers: auth_header(user), params: category_invalid_params          
+        end.to_not change(Category, :count)
+      end
+
+      it 'returns error message' do
+        post url, headers: auth_header(user), params: category_invalid_params          
+        expect(body_json['errors']['fields']).to have_key('name')
+      end
+
+      it 'returns unprocessable_entity status' do
+        post url, headers: auth_header(user), params: category_invalid_params          
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+    end
 
   end
+
 end
