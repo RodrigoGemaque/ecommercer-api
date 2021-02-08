22d1e4a0f3808ddeb4de29f51c8e4f73d46b50c6
Rodrigo <ro.gemaque16@gmail.com>
2021-02-08 06:27:50 -0300

    Rest routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       get "home" => "home#index"
       resources :categories
-      resources :coupons 
+      resources :coupons
+      resources :users 
     end
   end
 
