434ca93f7f67ff08598898b03ef280c074f65ace
Rodrigo <ro.gemaque16@gmail.com>
2021-02-08 16:28:52 -0300

    added field status in factory product

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -6,6 +6,7 @@ FactoryBot.define do
     # OCASIONAVA ERRO ṔOR NÃO TER UM PRODUTO JÁ CRIADO
     # productable { nil }
     image { Rack::Test::UploadedFile.new(Rails.root.join("spec/support/images/product_image.png")) }
+    status { :avaliable}
     after :build do |product|
       product.productable = create(:game)
     end
